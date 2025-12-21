fc3a35a8d2be2d8ab9b921441a2471a847a5d834
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2025-12-21 12:18:14 -0500

    chore: update vim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,13 +3,16 @@
 -- This is also a good place to setup other settings (vim.opt)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.opt.clipboard = "unnamedplus"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.shiftwidth = 4
 vim.opt.scrolloff = 8
+vim.opt.wrap = false
+vim.opt.list = true
 vim.opt.splitbelow = true
 vim.opt.splitright = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.list = true
 -- vim.opt.showmode = true (using mini.statusline)
