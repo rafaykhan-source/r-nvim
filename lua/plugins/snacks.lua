4e4277f49a3abeb855579d4348ce5c7c3b1e75a0
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2025-12-25 21:51:36 -0500

    chore: remove comments from snacks.lua

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,23 +4,13 @@ return {
   lazy = false,
   ---@type snacks.Config
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    -- bigfile = { enabled = true },
-    -- dashboard = { enabled = true },
+    picker = { enabled = true },
     explorer = { enabled = true, replae_netrw = true, trash = true },
     lazygit = { enabled = true },
-    -- indent = { enabled = true },
-    -- input = { enabled = true },
-    picker = { enabled = true },
-    -- notifier = { enabled = true },
-    -- quickfile = { enabled = true },
-    -- scope = { enabled = true },
-    -- scroll = { enabled = true },
-    -- statuscolumn = { enabled = true },
-    -- words = { enabled = true },
+    gh = { enabled = true },
+    image = { enabled = true },
   },
+
   keys = {
     { "<leader>f",  function() Snacks.picker.smart() end,        desc = "Search [F]iles (Smart)" },
     { "<leader>/",  function() Snacks.picker.grep() end,         desc = "Search Grep" },
