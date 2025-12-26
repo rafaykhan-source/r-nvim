7fe8826ffbfa83245f1e39893b5d781af622bf2f
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2025-12-25 20:57:41 -0500

    feat: add hardtime.lua

---
+++ lua/plugins/hardtime.lua
@@ -0,0 +1,9 @@
+return {
+  "m4xshen/hardtime.nvim",
+  lazy = false,
+  dependencies = { "MunifTanjim/nui.nvim" },
+  opts = {},
+  keys = {
+    { "<leader>h", ":Hardtime toggle<CR>", desc = "Toggle [H]ardtime" },
+  },
+}
