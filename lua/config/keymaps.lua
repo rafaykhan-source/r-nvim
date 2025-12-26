b0e30ab7f00e0b296706711d5554b05d8c3b5314
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2025-12-25 21:50:58 -0500

    chore: add descriptions to keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,21 +1,38 @@
 -- :so % (source current file)
-vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
-vim.keymap.set("n", "<leader>s", ":source %<CR>")
-vim.keymap.set("n", "<leader>q", function() vim.diagnostic.setloclist() end)
-vim.keymap.set("v", "<leader>x", ":lua<CR>")
-vim.keymap.set("n", "<leader>\\", ":vnew<CR>")
-vim.keymap.set("n", "<leader>l", ":Lazy<CR>")
-vim.keymap.set("n", "<leader>w", function() vim.lsp.buf.format() end)
 vim.keymap.set("n", "<C-k>", "<C-w><up>")
 vim.keymap.set("n", "<C-j>", "<C-w><down>")
 vim.keymap.set("n", "<C-h>", "<C-w><left>")
 vim.keymap.set("n", "<C-l>", "<C-w><right>")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<Esc>", ":nohlsearch<CR>")
 
--- Create a quick terminal
-vim.keymap.set("n", "<leader>t", function()
-  vim.cmd.vnew()
-  vim.cmd.terminal()
-end
+vim.keymap.set("n", "<leader>s", ":source %<CR>", { desc = "[S]ource Buffer" })
+vim.keymap.set("n", "<leader>l", ":Lazy update<CR>", { desc = "[L]azy Update" })
+vim.keymap.set("n", "<leader>q",
+  function()
+    vim.diagnostic.setloclist()
+  end,
+  { desc = "[Q]uick Diagnostic List" }
+)
+vim.keymap.set("n", "<leader>\\",
+  function()
+    vim.cmd.vnew()
+  end,
+  { desc = "New Split" }
+)
+vim.keymap.set("n", "<leader>w",
+  function()
+    vim.lsp.buf.format()
+    vim.cmd.update()
+  end,
+  { desc = "[W]rite and Format file" }
+)
+
+vim.keymap.set("n", "<leader>t",
+  function()
+    vim.cmd.vnew()
+    vim.cmd.terminal()
+  end,
+  { desc = "[T]erminal" }
 )
