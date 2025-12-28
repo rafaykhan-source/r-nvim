9636dc309ca636e425a7305d607fbe3bb37fd8e6
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2025-12-27 22:53:49 -0500

    chore: add search help to keymaps

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,13 +12,14 @@ return {
   },
 
   keys = {
-    { "<leader>f",  function() Snacks.picker.smart() end,        desc = "Search [F]iles (Smart)" },
-    { "<leader>/",  function() Snacks.picker.grep() end,         desc = "Search Grep" },
-    { "<leader>b",  function() Snacks.picker.buffers() end,      desc = "[B]uffers" },
-    { "<leader>c",  function() Snacks.picker.colorschemes() end, desc = "[C]olorschemes" },
-    { "<leader>u",  function() Snacks.picker.undo() end,         desc = "[U]ndo" },
-    { "<leader>gl", function() Snacks.lazygit() end,             desc = "[G]it [L]azy" },
-    { "<leader>e",  function() Snacks.explorer() end,            desc = "[E]xplorer" },
-    { "<leader>gi", function() Snacks.picker.gh_issue() end,     desc = "[G]itHub [I]ssues (open)" },
+    { "<leader>e",  function() Snacks.explorer() end,                     desc = "[E]xplorer" },
+    { "<leader>f",  function() Snacks.picker.files() end,                 desc = "Search [F]iles" },
+    { "<leader>/",  function() Snacks.picker.grep() end,                  desc = "Search Grep" },
+    { "<leader>b",  function() Snacks.picker.buffers() end,               desc = "[B]uffers" },
+    { "<leader>h",  function() Snacks.picker.help() end,                  desc = "[H]elp Pages" },
+    { "<leader>gl", function() Snacks.lazygit() end,                      desc = "[G]it [L]azy" },
+    { "<leader>gi", function() Snacks.picker.gh_issue() end,              desc = "[G]itHub [I]ssues (open)" },
+    { "<leader>c",  function() Snacks.picker.colorschemes() end,          desc = "[C]olorschemes" },
+    { "<leader>u",  function() Snacks.picker.undo() end,                  desc = "[U]ndo" },
   },
 }
