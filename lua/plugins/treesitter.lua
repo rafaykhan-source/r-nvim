b94d053c00aa5035a1c4a45e2dd691a1bfdf96fc
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2025-12-29 09:41:45 -0500

    feat: use newer treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,8 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  branch = 'master',
   lazy = false,
   build = ":TSUpdate",
   config = function()
-    require 'nvim-treesitter.configs'.setup {
-      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-      ensure_installed = { "lua", "vim", "vimdoc", "markdown", "markdown_inline", "python", "csv", "dockerfile", "ssh_config", "toml", "tmux", "yaml", "json", "bash"},
-    }
+    require 'nvim-treesitter'.install { "lua", "vim", "vimdoc", "markdown", "markdown_inline", "python", "csv", "dockerfile", "ssh_config", "toml", "tmux", "yaml", "json", "bash"}
   end,
 }
