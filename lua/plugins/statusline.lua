c1a597f834296516b7245a3d168be7b79b6b5106
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2025-12-30 17:16:23 -0500

    chore: load statusline

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,5 +1,5 @@
 return {
-  { 'nvim-mini/mini.statusline',  version = false },
+  { "nvim-mini/mini.statusline",  version = '*', config = function() require("mini.statusline").setup() end, },
   { "nvim-tree/nvim-web-devicons" },
   { "nvim-mini/mini.icons",       version = "*" },
 }
