51db0d3ea5d782327cf6fba00c1b3d1ab77b7fe7
Rafay <82721722+rafaykhan-source@users.noreply.github.com>
2026-01-18 15:20:32 -0500

    feat: update options

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,4 +3,6 @@ local set = vim.opt_local
 set.wrap = true
 set.textwidth = 80
 set.shiftwidth = 2
+set.colorcolumn = "80"
 set.showbreak = "***"
+set.spell = true
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,22 +1,38 @@
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
+
+-- Leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.opt.clipboard = "unnamedplus"
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+
+-- Editor
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.cursorline = true
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.colorcolumn = "120"
+vim.opt.winborder = "rounded"
 vim.opt.scrolloff = 16
 vim.opt.wrap = false
-vim.opt.list = true
-vim.opt.splitbelow = true
-vim.opt.splitright = true
 vim.opt.showmode = false
 vim.opt.termguicolors = true
+vim.opt.list = true
+
+-- Indentation
+vim.opt.shiftwidth = 4
+
+-- Search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Clipboard
+vim.opt.clipboard = "unnamedplus"
+
+-- Undo
 vim.opt.undofile = true
+vim.opt.swapfile = false
+
+-- Splits
+vim.opt.splitbelow = true
+vim.opt.splitright = true
