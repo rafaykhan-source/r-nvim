39be24f75d18172fe4abe0fd7f9493f5bf870456
Rafay K <82721722+rafaykhan-source@users.noreply.github.com>
2026-02-02 11:45:06 -0500

    chore: rename mini, use web-dev-icons

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -29,14 +29,10 @@ return {
     },
   },
   {
-    {
-      "nvim-mini/mini.statusline",
-      version = "*",
-      config = function()
-        require("mini.statusline").setup()
-      end,
-    },
-    { "nvim-mini/mini.icons", version = "*" },
-    -- { "nvim-tree/nvim-web-devicons" },
+    "nvim-mini/mini.statusline",
+    version = "*",
+    config = function()
+      require("mini.statusline").setup()
+    end,
   },
 }
